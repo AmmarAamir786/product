f025a590b315f45b8f381927b0f69aea94337c18
AmmarAamir786 <ammaraamir609@gmail.com>
2024-09-24 11:49:49 +0500

    fixed a lot of things

--- product_service/app/models/models.py
+++ product_service/app/models/models.py
@@ -2,7 +2,7 @@ from typing import Optional
 from sqlmodel import SQLModel, Field
 
 class Product (SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True, autoincrement=True)  # Ensure auto-increment
+    id: Optional[int] = Field(default=None, primary_key=True)  # Ensure auto-increment
     product_id: int
     name: str
     description: str
